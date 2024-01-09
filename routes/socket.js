2b6998cd8dddbd6f6423b0fce3e7ba61f903a11f
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-10 04:29:29 +0530

    Initial commit

---
+++ routes/socket.js
@@ -0,0 +1,26 @@
+module.exports = (io) => {
+  io.on("connection", (socket) => {
+    
+    socket.on("JoinRoom", (data) => {
+      socket.join(data.id);
+      const room = io.sockets.adapter.rooms.get(data.id);
+      if (room && room.size >= 2) {
+        io.to(data.id).emit("Joined", data.song);
+      }
+    });
+
+    socket.on("play", (data) => {
+      socket.to(data.id).emit("PlaySong", data.song);
+    });
+    socket.on("seek", (data) => {
+      socket.to(data.id).emit("seek", data.seek);
+    });
+
+    socket.on("disconnecting", () => {
+      const room = socket.rooms;
+      room.forEach((room) => {
+        if (room !== socket.id) socket.to(room).emit("UserLeft");
+      });
+    });
+  });
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+app.use(cors());
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: true,
+  },
+});
+
+require("./routes/socket")(io);
+
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
