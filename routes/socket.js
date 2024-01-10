e802f061533265dd33ade781400acf7b1641a054
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-11 01:15:15 +0530

    Update socket.js

--- routes/socket.js
+++ routes/socket.js
@@ -19,7 +19,7 @@ module.exports = (io) => {
     });
 
     socket.on("liked",(data)=>{
-      io.to(data.id).emit("like")
+      socket.to(data.id).emit("like")
     })
 
     socket.on("disconnecting", () => {
